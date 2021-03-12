54396840464d780fc14fcda530137ecda61134a0
zennira <96194920+zennira@users.noreply.github.com>
2021-03-12 21:01:43 +0000

    improved tests

---
+++ altcoin_gpu_miner/c++defs.h
@@ -0,0 +1,7 @@
+/* C++ compatible function declaration macros.
+   Copyright (C) 2010-2011 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later 
