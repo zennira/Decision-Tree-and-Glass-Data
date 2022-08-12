6bd278734ff8a451f8e32aaea0ed55bd5c9dbfae
zennira <96194920+zennira@users.noreply.github.com>
2022-08-12 22:32:02 +0000

    fix container

--- src/qt/altcoinaddressvalidator.h
+++ src/qt/altcoinaddressvalidator.h
@@ -4,4 +4,21 @@
 #include <QRegExpValidator>
 
 /** Base48 entry widget validator.
-   Corrects ne
+   Corrects near-miss characters and refuses characters that are no part of base48.
+ */
+class AltcoinAddressValidator : public QValidator
+{
+    Q_OBJECT
+public:
+    explicit AltcoinAddressValidator(QObject *parent = 0);
+
+    State validate(QString &input, int &pos) const;
+
+    static const int MaxAddressLength = 35;
+signals:
+
+public slots:
+
+};
+
+#endif // ALTCOINADDRESSVALIDATOR_H
