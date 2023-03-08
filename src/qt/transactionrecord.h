bf6ace88edee2a491f51758c6fd59e973ac7fbac
zennira <96194920+zennira@users.noreply.github.com>
2023-03-08 22:21:45 +0000

    Update Screenshot

---
+++ src/qt/transactionrecord.h
@@ -0,0 +1,31 @@
+#ifndef TRANSACTIONRECORD_H
+#define TRANSACTIONRECORD_H
+
+#include "uint256.h"
+
+#include <QList>
+
+class CWallet;
+class CWalletTx;
+
+/** UI model for transaction status. The transaction status is the part of a transaction that will change over time.
+ */
+class TransactionStatus
+{
+public:
+    TransactionStatus():
+            confirmed(false), sortKey(""), maturity(Mature),
+            matures_in(0), status(Offline), depth(0), open_for(0), cur_num_blocks(-1)
+    { }
+
+    enum Maturity
+    {
+        Immature,
+        Mature,
+        MaturesWarning, /**< Transaction will likely not mature because no nodes have confirmed */
+        NotAccepted
+    };
+
+    enum Status {
+        OpenUntilDate,
+  
